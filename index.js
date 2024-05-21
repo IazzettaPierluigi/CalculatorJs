4c9a5b39dec4ce443701a2e7880c85fd95bad4d7
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-05-21 16:22:28 +0200

    clean and print functions

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const display = document.getElementById("display");
 
 //function to print numbers and results in the display in HTML
 function appendToDisplay(input) {
-
+    display.value += input;
 }
 
 //function to clear display
 function clearDisplay() {
-
+    display.value = ""
 }
 
 
